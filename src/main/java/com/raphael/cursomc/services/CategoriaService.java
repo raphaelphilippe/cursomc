b102d81b47f9dab9d8c0a8929fa2550554fd5e5b
Raphael Philippe <almeidasilva.raphael@gmail.com>
2021-07-14 16:03:53 -0300

    Deletando uma Categoria com Delete

--- src/main/java/com/raphael/cursomc/services/CategoriaService.java
+++ src/main/java/com/raphael/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.raphael.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.raphael.cursomc.domain.Categoria;
 import com.raphael.cursomc.repositories.CategoriaRepository;
+import com.raphael.cursomc.services.exceptions.DataIntegrityException;
 import com.raphael.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -34,6 +36,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+		}
 	}
 	
 }
