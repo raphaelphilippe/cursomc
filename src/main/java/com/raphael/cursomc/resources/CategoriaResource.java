a621e7981c695de7b29ebc4d8f885e28aa3dcdcc
Raphael Philippe <almeidasilva.raphael@gmail.com>
2021-07-14 15:15:05 -0300

    Inserindo uma nova Categoriacom POST

--- src/main/java/com/raphael/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/raphael/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,18 @@
 package com.raphael.cursomc.resources;
 
+import java.net.URI;
+
 //import java.util.ArrayList;
 //import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.raphael.cursomc.domain.Categoria;
 import com.raphael.cursomc.services.CategoriaService;
@@ -27,4 +31,10 @@ public class CategoriaResource {
 		
 	}
 	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert (@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
